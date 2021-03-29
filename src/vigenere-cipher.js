03095314f3672730dc07a28abfe5a35ba2e8045b
nairabrab <dan.kobarov@gmail.com>
2021-03-29 19:27:40 +0300

    feat: add 100% working solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,31 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
+  constructor(directFlag = true) {
+    this.flag = directFlag;
+  }
+
   encrypt(message, key) {
-  if (! message && key) throw new Error;
-  // let keySet = new Set(key);
 
   function template (m, k) {
-    return m.split('').map((el,i) => (el >= 'a' && el <= 'z') ? k[i % k.length] : el).join('');
+    let i = 0;
+    return m.split('').map(el => (el >= 'a' && el <= 'z') ? k[ (i++) % k.length] : el).join('');
   }
-
   function encode(s,i) {
-    return  (s >= 'a' && s <= 'z') 
+    return  (s[i] >= 'a' && s[i] <= 'z') 
     ? ((s.charCodeAt(i)+substring.charCodeAt(i)-2*'a'.charCodeAt(0))%26 + 'a'.charCodeAt(0))
-    : ' '.charCodeAt(0);
-  }
-  // let substring = key;
-  // while (substring.length < message.length) {substring += key};
-  // substring = substring.slice(0, message.length);
-  let substring = template(message.toLowerCase(), key);
-
-  return String.fromCharCode(...message.split('').map((el,i) => encode(el,i)).join('')).toUpperCase();
+    : s.charCodeAt(i);
   }
 
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  if (! message && key) throw new Error;
+  message = message.toLowerCase();
+  key = key.toLowerCase();
+  let substring = template(message, key);
+
+  let result = String.fromCharCode(...message.split('').map((el,i) => encode(message,i))).toUpperCase();
+  if (!this.flag) result = result.split('').reverse().join('');
+  return result;
   }
+
+  decrypt(encryptedMessage, key) {
+    function template (m, k) {
+      let i = 0;
+      return m.split('').map(el => (el >= 'a' && el <= 'z') ? k[ (i++) % k.length] : el).join('');
+    }
+    function decode(s,i) {
+      return  (s[i] >= 'a' && s[i] <= 'z') 
+      ? ((s.charCodeAt(i)-substring.charCodeAt(i)+26)%26 + 'a'.charCodeAt(0))
+      : s.charCodeAt(i);
+    }
+
+    if (! encryptedMessage && key) throw new Error;
+    encryptedMessage = encryptedMessage.toLowerCase();
+    key = key.toLowerCase();
+
+    let substring = template(encryptedMessage, key);
+
+   
+  let result = String.fromCharCode(...encryptedMessage.split('').map((el,i) => decode(encryptedMessage,i))).toUpperCase();
+  if (!this.flag) result = result.split('').reverse().join('');
+  return result;
+ }
 }
 
 module.exports = VigenereCipheringMachine;
